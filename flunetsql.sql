6ea92caa4ec7b10ffbfba7126ea3044da18a43f4
David Wiltshire <david.c.a.wiltshire@gmail.com>
2023-03-24 11:19:56 -0400

    Add files via upload

--- flunetsql.sql
+++ flunetsql.sql
@@ -1,28 +1,37 @@
-/**CREATE TABLE flunet_table(
-   WHOREGION TEXT,
-   ITZ VARCHAR,
-   COUNTRYAREATERRITORY TEXT,
-   COUNTRY_CODE TEXT,
-   ORIGIN_SOURCE TEXT,	
-   ISO_YEAR	INT, 
-   ISO_WEEK INT,
-   ISO_SDATE DATE,
-   SPEC_RECEIVED_NB INT,
-	SPEC_PROCESSED_NB INT,
-	AH1 INT,
-	AH1N12009 INT,
-	AH3 INT,
-	AH5 INT,
-	ANOTSUBTYPED INT,
-	INF_A INT,
-	BVIC INT,
-	BYAM INT,
-	BNOTDETERMINED INT,
-	INF_B INT,
-	INF_ALL INT,
-	INF_NEGATIVE  INT,	
-	ILI_ACTIVITY INT
-
-);   **/
-
 SELECT * FROM flunet_table;
+
+--Comparing totals of "a" subtypes by year 
+SELECT iso_year,
+    SUM(ah1) AS ah1_sum,
+	SUM(ah1n12009) AS ah1n12009_sum,
+	SUM(ah3) AS ah3_sum,
+	SUM(ah5) AS ah5_sum,
+	SUM(anotsubtyped) AS anotsubtyped_sum
+FROM flunet_table
+GROUP BY iso_year
+ORDER BY iso_year;
+
+
+--Comparing totals of a and b subtypes by country/area/territory
+SELECT countryareaterritory,
+    SUM(inf_a) AS sum_all_a_subtypes,
+	SUM(inf_b) AS sum_all_b_subtypes
+FROM flunet_table
+GROUP BY countryareaterritory
+HAVING SUM(inf_a) > 0
+ORDER BY sum_all_a_subtypes DESC
+LIMIT 20;
+	
+
+--Finding the weeks of the year where infections are most prevalent
+SELECT iso_week,
+    SUM(inf_a) AS sum_all_a_subtypes,
+	SUM(inf_b) AS sum_all_b_subtypes
+FROM flunet_table
+GROUP BY iso_week
+HAVING SUM(inf_a) > 0
+ORDER BY sum_all_a_subtypes DESC
+LIMIT 10;
+
+
+/** Does the prevalence differ by region relative to weeks in the year? **/
